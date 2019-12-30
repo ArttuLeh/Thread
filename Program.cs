2d73601af766c1188633e4bbe1697583af4a962c
ArttuLeh <lehtovaara.arttu@gmail.com>
2019-12-30 15:07:20 +0200

    commit

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿/**********************************
+ *                                *
+ * Arttu Lehtovaara, 1701848      *
+ *                                *
+ **********************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread_version
+{
+    class Program
+    {
+        static int products_lenght;
+        static int thread_count;
+        static int[] products;
+        static Mutex mutex = new Mutex();
+
+        static int findFreeIndex(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 0) // etsitään tyhjä paikka, tyhjä paikka = 0
+                {
+                    return i;
+                }
+            }
+            return -1; // palautetaan negatiivinen arvo jos ei löytynyt,
+            // koska index ei voi olla negatiivinen arvo
+            
+        }
+
+        static void producer(object data)
+        {
+            // thread id will be 1, 2... (delivered in Thread.Start(i + 1))
+            
+            int tid = (int)data;
+
+            while (true)
+            {
+                // lock mutex
+                mutex.WaitOne();
+                
+                int index = findFreeIndex(products);
+                // if found --> write my tid to found index
+                if (index >= 0)
+                {
+                    products.SetValue(tid, index);
+                    //products[index] = tid;
+                }
+                mutex.ReleaseMutex();
+                //if not found-- > break
+                if (index == -1)
+                    break;
+                Thread.Sleep(100); // kokeilin sleep:iä käyttäen
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // check first the args count
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: <prog> <product_count> <thread_count>");
+                Console.ReadLine();
+                return;
+            }
+            // convert args to int.. use default values if illegal
+            if (!int.TryParse(args[0], out products_lenght))
+            {
+                products_lenght = 100;
+            }
+            if (!int.TryParse(args[1], out thread_count))
+            {
+                thread_count = 10;
+            }
+            // now we have legal values, let's create the array
+            products = new int[products_lenght];
+            // let's create the threads..
+            Thread[] threads = new Thread[thread_count];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(producer);
+            }
+            // and run the threads
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start(i + 1);
+            }
+            // and wait for their finish
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+            // and finally calculate statistics
+            int[] products_count = new int[thread_count + 1];
+            // let's temporarily emulate the results
+            //Random r = new Random();
+            //for (int i = 0; i < products.Length; i++)
+            //{
+            //    products[i] = r.Next(1, thread_count + 1);
+            //}
+            // for loop where all the products counted by producer id
+            for (int i = 0; i < products.Length; i++)
+            {
+                products_count[products[i]]++;
+            }
+            foreach (var item in products)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < products_count.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write("- ");
+                }
+                else
+                {
+                    Console.Write(products_count[i] + " ");
+                }
+            }
+            //foreach (var item in products_count)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            Console.WriteLine();
+            
+        }
+    }
+}
